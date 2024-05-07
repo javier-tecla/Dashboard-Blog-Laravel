b5172295ad6fd609b88f12f6f86712acf757a984
javierBorjas <cristman11@gmail.com>
2024-05-07 12:56:50 -0300

    Routes

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/** 
+ * Route::get       |Consultar
+ * Route::post      |Guardar
+ * Route::delete    |Eliminar
+ * Route::put       |Actualizar
+*/
+
+Route::get('/', function () {
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // consulta a base de datos 
+
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
+});
