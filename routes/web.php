919ac739148e1d93daa200f99398345952420a15
javierBorjas <cristman11@gmail.com>
2024-05-07 16:33:35 -0300

    Inicio de sesión parte administrativa

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PageController;
+
 
 /** 
  * Route::get       |Consultar
@@ -25,3 +28,15 @@ Route::controller(PageController::class)->group(function () {
     Route::get('blog',         'blog')->name('blog');
     Route::get('blog/{post:slug}',  'post')->name('post');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
