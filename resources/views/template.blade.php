3f19a35fc685eb30d850e5f4888297c41fcb954d
javierBorjas <cristman11@gmail.com>
2024-05-09 15:44:28 -0300

    Diseño Web

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -5,21 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Proyecto web</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
-    <p>
-        <a href="{{ route('home') }}">Home</a>
-        <a href="{{ route('blog') }}">Blog</a>
+    <div class="container px-4 mx-auto">
+        <header class="flex justify-between items-center py-4">
+            <div class="flex items-center flex-grow gap-4">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('elefante.png')}}" class="h-12">
+                </a>
+                <form action="">
+                    <input type="text" placeholder="Buscar">
+                </form>
+            </div>
 
-        @auth
+            @auth
         <a href="{{ route('dashboard')}}">Dashboard</a>
         @else
         <a href="{{ route('login')}}">Login</a>
         @endauth
-    </p>
+        </header>
 
-    <hr>
-
-    @yield('content')
+        @yield('content')
+    </div>
 </body>
 </html>
