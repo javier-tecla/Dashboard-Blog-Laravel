507e105e48ab3f077d4fd6b70afb84e3d6d2b3f6
javierBorjas <cristman11@gmail.com>
2024-05-10 00:29:18 -0300

    optimizacion final

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                        {{ __('Posts') }}
                     </x-nav-link>
                 </div>
             </div>
@@ -34,10 +37,6 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        {{-- <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link> --}}
-
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
@@ -67,24 +66,22 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('home')">
+                {{ __('Home') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
+                {{ __('Posts') }}
             </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
-                {{-- <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div> --}}
-                {{-- <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div> --}}
+                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
-                {{-- <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link> --}}
-
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 
-/** 
- * Route::get       |Consultar
- * Route::post      |Guardar
- * Route::delete    |Eliminar
- * Route::put       |Actualizar
- */
-
-/** 
-Route::get('/', [PageController::class, 'home'])->name('home');
-
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
-
-Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
-*/
-
-// Otra manera de escribirlo
 Route::controller(PageController::class)->group(function () {
 
     Route::get('/',            'home')->name('home');
-    Route::get('blog/{post:slug}',  'post')->name('post');
+    Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::redirect('dashboard', 'posts')->name('dashboard');
 
-Route::resource('posts', PostController::class)->except(['show']);
+Route::resource('posts', PostController::class)->middleware(['auth'])->except(['show']);
 
 require __DIR__.'/auth.php';
